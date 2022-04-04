b882e6ad3439865f2200f5d1ee2e35fbe18f28ca
robozavri <nika37912@hotmail.com>
2022-04-05 03:30:50 +0400

    [feat][clinet-home]: add section why-pide component:

--- client/src/app/routes/home/home.module.ts
+++ client/src/app/routes/home/home.module.ts
@@ -5,11 +5,12 @@ import { HomeRoutingModule } from './home-routing.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ComponentsModule } from 'src/app/components/components.module';
 import { PromoComponent } from './components/promo/promo.component';
+import { WhyPideComponent } from './components/why-pide/why-pide.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent, PromoComponent],
+  declarations: [HomeComponent, PromoComponent, WhyPideComponent],
   imports: [
     CommonModule,
     HomeRoutingModule,
